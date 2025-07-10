c1ef0f7111d92957b03e4c6b60d66f7eb37a0c40
vanraak <129201527+vanraak@users.noreply.github.com>
2025-07-10 17:36:31 +0200

    minor changes

--- convert_rdata.py
+++ convert_rdata.py
@@ -2,21 +2,23 @@ import pandas as pd
 import os
 import pyreadr
 
-os.chdir(r"c:\Surfdrive\Data Science Textbook\data")
-for file in os.listdir():
+data_dir = os.path.join(os.getcwd(), "sourcedata")
+
+for file in os.listdir(data_dir):
     if file.endswith('.RData'):
-        filename=str(file).split('.')[0]
-        result = pyreadr.read_r(file)
+        filename = os.path.splitext(file)[0]
+        file_path = os.path.join(data_dir, file)
+        result = pyreadr.read_r(file_path)
         df=pd.DataFrame(result[filename])
         df.columns= [col.replace('.','_').replace(' ','_').lower() for col in df.columns]
         filename=filename.lower()
         df.to_csv(f'csv\\{filename}.csv', index=False)
         df.to_pickle(f'pickle\\{filename}.pkl')
-        df.to_parquet(f'parquet\\{filename}.parquet')
+        df.to_parquet(f'dsf\\data\\{filename}.parquet')
 
 #Separately convert the corona dataset
-df = pd.read_excel('COVID-19-geographic-disbtribution-worldwide.xlsx')
+df = pd.read_excel(data_dir+'/COVID-19-geographic-disbtribution-worldwide.xlsx')
 df.columns= [col.replace('.','_').replace(' ','_').lower() for col in df.columns]
 df.to_csv('csv\\corona.csv', index=False)
 df.to_pickle('pickle\\corona.pkl')
-df.to_parquet('parquet\\corona.parquet')
+df.to_parquet('dsf\\data\\corona.parquet')
