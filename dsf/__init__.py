1486f5487ecdc4a3fd6e519d7d8f9bb58382b0f1
vanraak <129201527+vanraak@users.noreply.github.com>
2025-09-20 14:10:16 +0200

    Edit

--- dsf/__init__.py
+++ dsf/__init__.py
@@ -2,7 +2,7 @@ from ._version import __version__
 from .datasets import load, datasets, dataset_table
 from .hybrid_nb import HybridNB
 
-__all__ = ["HybridNB", "load", "datasets", "dataset_table"]
+__all__ = ["load", "datasets", "dataset_table"]
 
 __doc__ = f"""
 DSF package: Example datasets for Python users
@@ -15,19 +15,3 @@ Available datasets:
 >>> import dsf
 >>> df = dsf.load('<dataset_name>')  # Load a dataset as a pandas DataFrame
 >>> dsf.version()  # DSF version {__version__}
-
-
-# Example usage of HybridNB
-
->>> from dsf import HybridNB
->>> hnb = HybridNB(
-...     num_cols=num_cols,               # continuous numeric features
-...     cat_cols=multinomial_cat_cols,   # categorical (Multinomial)
-...     count_cols=count_cols,           # counts (MultinomialNB)
-...     bernoulli_cols=bernoulli_cols,   # binary (BernoulliNB)
-...     alpha=1.0
-... )
->>> hnb.fit(X_train, y_train)
->>> y_pred = hnb.predict(X_test)
->>> y_proba = hnb.predict_proba(X_test)
-"""
